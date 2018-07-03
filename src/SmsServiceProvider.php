0584905271e74268eb0ca9aa7736bdabd4f50f13
Xu Tongle <xutongle@gmail.com>
2018-07-03 11:46:09 +0800

    init

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace XuTL\Sms;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * SMS服务提供者
+ * @package XuTL\Sms
+ */
+class SmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/sms.php' => config_path('sms.php'),
+        ]);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerLaravelBindings();
+    }
+
+    /**
+     * Register Laravel bindings.
+     *
+     * @return void
+     */
+    protected function registerLaravelBindings()
+    {
+        $this->app->singleton(EasySms::class, function ($app) {
+            return new EasySms($app['config']['sms']);
+        });
+    }
+}
