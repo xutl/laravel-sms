467919aee70d88ca9e5d10fad376ccf3072af906
Xu Tongle <xutongle@gmail.com>
2018-07-03 13:57:50 +0800

    update

--- config/sms.php
+++ config/sms.php
@@ -1,20 +1,54 @@
 <?php
 
+use Overtrue\EasySms\Strategies\OrderStrategy;
+
 return [
     // HTTP 请求的超时时间（秒）
     'timeout' => 5.0,
 
-    // 默认发送配置
+    /*
+    |--------------------------------------------------------------------------
+    | Default Setting
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default sms gateway that gets used when writing
+    | messages to the sms. The name specified in this option should match
+    | one of the gateways defined in the "gateways" configuration array.
+    |
+    */
     'default' => [
         // 网关调用策略，默认：顺序调用
-        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+        'strategy' => OrderStrategy::class,
 
-        // 默认可用的发送网关
+        /*
+        |--------------------------------------------------------------------------
+        | Default Gateways
+        |--------------------------------------------------------------------------
+        |
+        | This option defines the default sms gateway that gets used when writing
+        | messages to the sms. The name specified in this option should match
+        | one of the gateways defined in the "gateways" configuration array.
+        |
+        */
         'gateways' => [
 
         ],
     ],
-    // 可用的网关配置
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log Gateways
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the sms gateways for your application.  This gives
+    | you a variety of powerful log handlers / formatters to utilize.
+    |
+    | Available Drivers: "aliyun", "alidayu", "yunpian", "submail",
+    |                    "errorlog", "luosimao", "yuntongxun", "huyi"
+    |                    "juhe", "sendcloud", "baidu", "huaxin", "chuanglan"
+    |                    "rongcloud", "tianyiwuxian", "twilio", "qcloud", "avatardata"
+    |
+    */
     'gateways' => [
         //Doc
         // https://github.com/overtrue/easy-sms
